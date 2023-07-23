bd3f237364470eef40b8de444b6fab08c2baae86
TheBinaryLife <aritrachatterjee2007@gmail.com>
2023-07-23 09:25:38 +0530

    JFB_Tut10_Codes

--- Operators-Part1/src/org/tbl/OperatorsDemo.java
+++ Operators-Part1/src/org/tbl/OperatorsDemo.java
@@ -1,24 +1,27 @@
 package org.tbl;
 public class OperatorsDemo {
     public static void main(String[] args) {
-        //Assignment operator
+        //Assignment Operator
         int val1;
-        val1 = 10;
+        val1 = 10 + 5;
         System.out.println(val1);
 
         //Negation Operator
         int val2;
-        val2 = -10;
+        val2 = 10;
+        val2 = -val2;
+        System.out.println(val2);
         val2 = -val2;
         System.out.println(val2);
 
         //Complement Operator
-        boolean val3;
-        val3 = false;
+        boolean val3 = false;
+        val3 = !val3;
+        System.out.println(val3);
         val3 = !val3;
         System.out.println(val3);
 
-        //++, -- Prefix operators
+        //Prefix operator
         int val4 = 10;
         //val4 = val4 + 1;
         ++val4;
@@ -27,16 +30,23 @@ public class OperatorsDemo {
         --val4;
         System.out.println(val4);
 
-        //Postfix operators ++, --
-        int val5 = 5;
-        //int val6 = val5;
-        //val5 = val5 + 1;
-        int val6 = val5++;
-        System.out.println("val5 = " + val5 + " val6 = " + val6);
+        //Postfix operator
+        int val5 = 15;
+        //val5 = val5 - 1
+        val5++;
+        System.out.println(val5);
+        //val5 = val5 - 1
+        val5--;
+        System.out.println(val5);
 
-        //int val7 = val5;
-        //val5 = val5 - 1;
-        int val7 = val5--;
-        System.out.println("val7 = " + val7 + " val5 = " + val5) ;
+        int val6 = 15;
+        int output = val6++;
+        System.out.println(output);
+        System.out.println(val6);
+
+        int val7 = 20;
+        int output2 = val7--;
+        System.out.println(output2);
+        System.out.println(val7);
     }
 }
